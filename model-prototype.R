eb5931fe21cd3cd708f3b5be6610f8c18b4a61a7
Etoile Wren Tanaka Boots <148799119+etoileboots@users.noreply.github.com>
2025-05-20 15:42:23 -0700

    Commiting main working document

--- model-prototype.R
+++ model-prototype.R
@@ -1,6 +1,8 @@
 # Import required libraries
-Sys.setenv(GITHUB_PAT = "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")
+# Install this if you don't have devtools.
+install.packages("devtools")
 devtools::install_github("css4s/socmod")
+
 library(ggnetwork)
 library(purrr)
 library(magrittr)
@@ -17,32 +19,23 @@ small_world_graph <- make_small_world(N, k, p = 0.2)
 plot(small_world_graph)
 
 abm <- make_abm(graph = small_world_graph)
-# Initialize 20% (4) agents with adaptive behvaior w/ 
+# Initialize 20% (4) agents with adaptive behavior w/ 
 # an adaptive fitness 1.2x of non-adaptive legacy behavior
 initialize_agents(abm, initial_prevalence = 0.2, adaptive_fitness = 1.2)
-# Visually inspect the initialization to see that 
-# N*p agents have adopted adaptive behavior
-p <- plot_network_adoption(
-  abm, layout = igraph::in_circle(),
-  plot_mod = \(p)p + ggtitle("Adoption of Adaptive Behavior at t=0",
-                             edgewidth=0.75)
-)
-p <- socmod::plot_network_adoption(
-  abm, layout = igraph::in_circle(), 
-  plot_mod = \(p) p + ggtitle("Adoption at t = 0"), edgewidth = 0.75
-)
+
 # Check table of behaviors to count them
 behaviors <- purrr::map_vec(abm$agents, \(a) a$get_behavior())
 table(behaviors)
+
 # Check initial adoption and network
 plot_network_adoption(abm)
 
 # Run a trial and visualize results
-
-trial <- run_trial(abm, stop = 10)
+trial <- run_trial(abm, stop = socmod::fixated)
 trial$get_outcomes()
-trial$get_observations()
+print(trial$get_observations(), n=20)
 
+# Analyse the prevalence of adaptive behavior across trials 
 prevalence_summary <- summarise_prevalence(trial)
 
 plot_prevalence(trial, tracked_behaviors = "Adaptive")
@@ -51,6 +44,7 @@ class(trial)
 # trials will be like...
 list(trial, trial, trial)
 
+# Commenting this out for right now 
 prototype_generator <- function(parameter_row) {
   N <- parameter_row$N; k <- parameter_row$k; p <- parameter_row$rewire_prob 
   
@@ -63,7 +57,7 @@ prototype_generator <- function(parameter_row) {
     do.call(make_abm, parameter_row) %>% initialize_agents
   )
 }
-
+# Alter the probability. of rewiring 
 rewire_prob_vals <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1.0)
 trials <- run_trials(
   # First argument: model generator function
@@ -73,9 +67,9 @@ trials <- run_trials(
   N = 50,
   k = 10,
   rewire_prob = rewire_prob_vals,
-  stop = socmod::fixated,
   # Trial metadata/metaparameters
   n_trials_per_param = 10,
+  stop = socmod::fixated,
   
   # System configuration
   .progress = TRUE,
@@ -83,7 +77,27 @@ trials <- run_trials(
   syncfile = "save-progress.RData"
 )
 
-plot_prevalence(trials[[35]])
+# Summarize within trials only; rename and factor rewire probability for display
+summary <- summarise_prevalence(
+  trials, input_parameters = "rewire_prob", across_trials = F
+) %>% 
+  dplyr::mutate(
+    `Rewire Probability` = factor(rewire_prob, rewire_prob_vals)
+  )
+
+p <- ggplot(
+  summary,
+  aes(x=Step, y=Prevalence,
+      color=`Rewire Probability`,
+      linetype=`Rewire Probability`,
+      group = trial_id)) +
+  geom_line(linewidth=1.4, alpha = 0.875) + theme_classic(base_size = 16) +
+  ggplot2::scale_color_manual(values = unname(SOCMOD_PALETTE))
+
+p
+
+# We've got 60 trials total 
+length(trials)
 
 outcomes <- summarise_outcomes(
   trials, 
@@ -108,12 +122,10 @@ outcomes_norm$Measure <- factor(outcomes_norm$Measure, levels = c(
 # Use a custom socmod line color
 line_color <- SOCMOD_PALETTE_CVD["pink"]
 outcomes_norm %>%
-  ggplot(aes(x = rewire_prob, y = Value, linetype = Measure)) +
+  ggplot2::ggplot(aes(x = adaptive_fitness, y = Value, linetype = Measure)) +
   geom_line(color = line_color, linewidth=1.5) + 
-  scale_x_continuous(breaks = rewire_prob_vals) +
-  theme_classic(base_size = 16) + 
-  xlab("Rewire probability") + ylab("Value")
-
+  scale_x_continuous(breaks = adaptive_fitness_vals) +
+  theme_classic(base_size = 16) + xlab("Adaptive fitness") + ylab("Value")
 
 
 
